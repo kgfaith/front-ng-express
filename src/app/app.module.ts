d1d549b2999c3aceac87e5061bbb6d7750cba843
Kyaw Kyaw Win <kg.faith@gmail.com>
2018-01-09 14:12:04 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {AuthModule} from "./auth/auth.module";
+
+export const ROUTES: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'auth'
+    }
+];
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(ROUTES),
+        AuthModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
